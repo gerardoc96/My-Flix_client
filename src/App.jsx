9243934c67964c55a4a7ad261615ec114eea2125
Carlos <61396980+gerardoc96@users.noreply.github.com>
2025-04-20 19:44:55 -0700

    Set up initial routing logic for LoginPage

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import MovieListPage from './pages/MovieListPage';
+import MovieDetailsPage from './pages/MovieDetailspage';
+import ProfilePage from './pages/ProfilePage';
+import Navbar from './components/navbar/Navbar';
+import PrivateRoute from './components/PrivateRoute/PrivateRoute';
+
+
+export function App() {
+  return (
+    <>
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+
+        {/* Private Routes */}
+        <Route
+          path="/movies"
+          element={
+            <PrivateRoute>
+              <MovieListPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/movies/:movieId"
+          element={
+            <PrivateRoute>
+              <MovieDetailsPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/profile"
+          element={
+            <PrivateRoute>
+              <ProfilePage />
+            </PrivateRoute>
+          }
+        />
+        {/* Fallback Routes */}
+        <Route path="/" element={<Navigate to="/movies" />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  )
+}
--- src/index.jsx
+++ src/index.jsx
@@ -3,18 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router';
+import App from './App';
 
-import "./index.scss";
-
-const MyFlixApplication = () => {
-  return (
-    <div className="my-flix">
-      <div>Good morning</div>
-    </div>
-  );
-};
-
-const container = document.querySelector("#root");
-const root = createRoot(container);
-
-root.render(<MyFlixApplication />);
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+);
